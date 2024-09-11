23d55eedfc5930023248770458d12377d131c5d8
XCode-tech <xcode.tech101@gmail.com>
2024-09-11 12:42:14 +0530

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, Suspense } from 'react';
-import Meta from '../components/ui/Meta';
+import Meta from '@/components/ui/Meta';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
