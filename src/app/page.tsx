0d88da72ccf0f7536949ffcf6a16738147beb197
XCode-tech <xcode.tech101@gmail.com>
2024-09-16 12:21:26 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Component from "@/pages/Component";
+// import Component from "@/pages/Component";
+import { Component } from "@/pages/Component";
 
 
 export default function Home() {
